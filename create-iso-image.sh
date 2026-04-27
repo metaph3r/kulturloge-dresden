2e1f6bf74da7c5f54783c882f1a037f6f49d9f18
Silvio Glöckner <silvio.gloeckner@adesso.de>
2026-04-27 09:27:06 +0200

    added script to create the iso image

---
+++ create-iso-image.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+AUTOINSTALL_FILE=./autoinstall.yaml
+SOURCE_DIR=./ubuntu
+TARGET_ISO=./iso/ubuntu-24.04.4-kulturloge-autoinstall-desktop-amd64.iso
+VOLUME_NAME=UBUNTU_KULTURLOGE
+
+# delete old image
+echo "Deleting old image..."
+rm $TARGET_ISO
+
+# create new image
+echo "Copying autoinstall.yaml to source location..."
+cp $AUTOINSTALL_FILE $SOURCE_DIR
+echo "Creating iso image $TARGET_ISO"
+xorriso -as mkisofs -r -V $VOLUME_NAME -o $TARGET_ISO -J -l -b boot/grub/i386-pc/eltorito.img -c boot.catalog -no-emul-boot -boot-load-size 4 -boot-info-table $SOURCE_DIR
